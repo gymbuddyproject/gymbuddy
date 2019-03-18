4713c5b8b38ec64b55f49f6a61f63cf60f227875
Patrick Devanney <2329979d@student.gla.ac.uk>
2019-03-18 17:36:08 +0000

    Updated Models.py - Comment now automatically gets time, other stuff changed as well

--- gymbuddy/models.py
+++ gymbuddy/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Gym(models.Model):
     GymName = models.CharField(max_length=30)
@@ -20,12 +21,13 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     AboutMe = models.TextField(max_length=300, blank=True)
-    Following = models.CharField(max_length = 20, blank=True)
     ProfilePicture = models.ImageField(upload_to='profile_images', blank=True)
     GymID = models.ForeignKey(Gym, blank=True)
+    Following = models.CharField(max_length=300, blank=True, on_delete=models.CASCADE)
     Height = models.DecimalField(max_length=3, decimal_places=2, max_digits=5)
     Weight = models.IntegerField()
     DoB = models.DateField()
+
     experience_choices = (('Beginner', 'Beginner'), ('Intermediate', 'Intermediate'), ('Advanced', 'Advanced'))
     Experience = models.CharField(max_length=12, choices=experience_choices, default='Beginner')
 
@@ -37,7 +39,7 @@ class Profile(models.Model):
 
 class ProgressPics(models.Model):
     PhotoID = models.AutoField(primary_key=True)
-    UserName = models.ForeignKey(User)
+    UserName = models.ForeignKey(Profile, on_delete=models.CASCADE)
     Photo = models.ImageField(upload_to='progress_pics')
     Likes = models.IntegerField(default=0)
     
@@ -46,9 +48,9 @@ class ProgressPics(models.Model):
 
 class Comments(models.Model):
     CommentID = models.AutoField(primary_key=True)
-    Poster = models.ForeignKey(Profile)
-    OnPic = models.ForeignKey(ProgressPics)
-    Date = models.DateTimeField(auto_now_add=True)
+    Poster = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    OnPic = models.ForeignKey(ProgressPics, on_delete=models.CASCADE)
+    Date = models.DateTimeField(default=timezone.now)
     Comment = models.CharField(max_length=200)
 
     class Meta:
