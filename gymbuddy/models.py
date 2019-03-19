add175ca3193dd78dc61b16984ac75e1863918d3
Joshua White <2315437W@student.gla.ac.uk>
2019-03-19 16:07:14 +0000

    Josh - population script

--- gymbuddy/models.py
+++ gymbuddy/models.py
@@ -3,6 +3,7 @@ from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
 class Gym(models.Model):
     GymName = models.CharField(max_length=30)
     Address = models.CharField(max_length=30)
@@ -10,13 +11,14 @@ class Gym(models.Model):
     Y_Coord = models.DecimalField(max_length=10, decimal_places=6, max_digits=9)
     Rating = models.DecimalField(max_length=3, decimal_places=2, max_digits=4)
     OpeningHours = models.CharField(max_length=50)
-    
+
     class Meta:
-      verbose_name_plural = "Gyms"
-      
+        verbose_name_plural = "Gyms"
+
     def __str__(self):
-        return self.GymName 
-        
+        return self.GymName
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
@@ -32,19 +34,21 @@ class Profile(models.Model):
     Experience = models.CharField(max_length=12, choices=experience_choices, default='Beginner')
 
     class Meta:
-      verbose_name_plural = "Profiles"
+        verbose_name_plural = "Profiles"
 
     def __str__(self):
-      return self.user.username
+        return self.user.username
+
 
 class ProgressPics(models.Model):
     PhotoID = models.AutoField(primary_key=True)
     UserName = models.ForeignKey(Profile, on_delete=models.CASCADE)
     Photo = models.ImageField(upload_to='progress_pics')
     Likes = models.IntegerField(default=0)
-    
+
     class Meta:
-      verbose_name_plural = "ProgressPics"
+        verbose_name_plural = "ProgressPics"
+
 
 class Comments(models.Model):
     CommentID = models.AutoField(primary_key=True)
@@ -54,4 +58,4 @@ class Comments(models.Model):
     Comment = models.CharField(max_length=200)
 
     class Meta:
-      verbose_name_plural = "Comments"
+        verbose_name_plural = "Comments"
