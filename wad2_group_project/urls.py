5397fa25f85ca351c7975c29f445ad1bf124bf85
MatthewNeilson <matthewneilson.99@gmail.com>
2019-03-20 12:10:50 +0000

    google maps slug link works kinda

--- wad2_group_project/urls.py
+++ wad2_group_project/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
     url(r'^test/', views.test, name='test'),
-    url(r'^gymprofile/', views.gymprofile, name='gymprofile'),
 	url(r'^gym/(?P<gym_slug>[\w\-]+)/$', views.gymprofile, name='gym_profile'),
     url(r'^userprofile/', views.userprofile, name='userprofile'),
     url(r'^contactus/', views.contactus, name='contactus'),
