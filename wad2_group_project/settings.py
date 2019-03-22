3c9677ba3a7f4531f9a9696bfae9e470d4007d04
Calum Davidson <calumdavi123@gmail.com>
2019-03-22 14:08:24 +0000

    static update 2

--- wad2_group_project/settings.py
+++ wad2_group_project/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATICFILES_DIRS = [STATIC_ROOT, ]
 
 
 #Media files
